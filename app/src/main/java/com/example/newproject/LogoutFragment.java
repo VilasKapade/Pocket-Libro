850e19e8e418b2626e569337ed080dee77034c5e
VilasKapade <kapadevilas2003@gmail.com>
2024-01-02 18:48:15 +0530

    finshed Project

--- app/src/main/java/com/example/newproject/LogoutFragment.java
+++ app/src/main/java/com/example/newproject/LogoutFragment.java
@@ -59,6 +59,6 @@ public class LogoutFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_logout, container, false);
+        return inflater.inflate(R.layout.fragment_audio_book, container, false);
     }
 }
